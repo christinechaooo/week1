79e4ac4fe32426a284e547a26de1eb58777ca796
Christine Chao <christinechaooo@gmail.com>
2014-06-06 18:22:05 -0700

    first commit

---
+++ week1/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  week1
+//
+//  Created by Christine Chao on 6/6/14.
+//  Copyright (c) 2014 Christine Chao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
---
+++ week1/MainViewController.m
@@ -0,0 +1,38 @@
+//
+//  MainViewController.m
+//  week1
+//
+//  Created by Christine Chao on 6/6/14.
+//  Copyright (c) 2014 Christine Chao. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
